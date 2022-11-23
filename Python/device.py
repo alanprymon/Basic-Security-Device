1688767176e6c68e21da95a3acf0a6d73a29a790
Alan <aprymon@hawk.iit.edu>
2022-11-22 18:10:36 -0600

    removed use less check

--- Python/device.py
+++ Python/device.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     while True:
         get = input()
 
+
+
         if not get.isdecimal():
             # remove every character that is not a decimal (0-9)
             checked = get
@@ -18,29 +20,27 @@ if __name__ == "__main__":
             delete.clear()
             get = checked
 
-        if not len(get) == 0:
-            # makes sure something still remains in the string
-            for x in get:
-                # handle the input string into individual characters
-                if x == "1" and idx == 5:
-                    if not unlocked:
-                        unlocked = True
-                        print("unlocked")
-                        idx = 0
-                elif x == "4" and idx == 5:
-                    if unlocked:
-                        unlocked = False
-                        print("locked")
-                        idx = 0
-                elif x == "8" and idx == 0 or idx == 3:
-                    idx += 1
-                elif x == "3" and idx == 1 or idx == 4:
-                    idx += 1
-                elif x == "9" and idx == 2:
-                    idx += 1
-                elif x == "8" and idx > 3 or idx < 3:
-                    idx = 1
-                elif x == "9" and idx == 5:
-                    idx = 3
-                else:
+        for x in get:
+            # handle the input string into individual characters
+            if x == "1" and idx == 5:
+                if not unlocked:
+                    unlocked = True
+                    print("unlocked")
                     idx = 0
+            elif x == "4" and idx == 5:
+                if unlocked:
+                    unlocked = False
+                    print("locked")
+                    idx = 0
+            elif x == "8" and idx == 0 or idx == 3:
+                idx += 1
+            elif x == "3" and idx == 1 or idx == 4:
+                idx += 1
+            elif x == "9" and idx == 2:
+                idx += 1
+            elif x == "8" and idx > 3 or idx < 3:
+                idx = 1
+            elif x == "9" and idx == 5:
+                idx = 3
+            else:
+                idx = 0
