5186fa4bd8aba2bcebdd4bbb716eff51ee66ccc8
Alan <aprymon@hawk.iit.edu>
2022-11-22 19:11:31 -0600

    added data worker
    * reworked what data.txt contains
    * added raw-data.txt
    * added dataGetter

---
+++ Python/dataGetter.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    rawdata = open("raw-data.txt", "r")
+    data = open("data.txt", "w")
+    raw = rawdata.readlines()
+    rawdata.close()
+    for x in range(len(raw)):
+        raw[x] = int(raw[x])
+    average = sum(raw) / len(raw)
+    data.write("Average: " + str(average) + "\n")
+    data.write("Min: " + str(min(raw)) + "\n")
+    data.write("Max: " + str(max(raw)) + "\n")
+    data.write("Total number of tests: " + str(len(raw)))
+    data.close()
