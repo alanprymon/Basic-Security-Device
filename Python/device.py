48d788e2ee46adb26b61a222eb24e9099d9e885b
Alan <aprymon@hawk.iit.edu>
2022-11-22 23:23:01 -0600

    more unit test compatibility

--- Python/device.py
+++ Python/device.py
@@ -48,27 +48,6 @@ if __name__ == "__main__":
 
         get = only_number(get)
 
-        for x in get:
-            # handle the input string into individual characters
-            if x == "1" and idx == 5:
-                if not unlocked:
-                    unlocked = True
-                    print("unlocked")
-                    idx = 0
-            elif x == "4" and idx == 5:
-                if unlocked:
-                    unlocked = False
-                    print("locked")
-                    idx = 0
-            elif x == "8" and idx == 0 or idx == 3:
-                idx += 1
-            elif x == "3" and idx == 1 or idx == 4:
-                idx += 1
-            elif x == "9" and idx == 2:
-                idx += 1
-            elif x == "8" and idx > 3 or idx < 3:
-                idx = 1
-            elif x == "9" and idx == 5:
-                idx = 3
-            else:
-                idx = 0
+        sent = check_unlock(get, idx, unlocked)
+        idx = sent[0]
+        unlocked = sent[1]
--- Python/test_device.py
+++ Python/test_device.py
@@ -1,11 +1,15 @@
 import unittest
 import device
 
-
 class MyTestCase(unittest.TestCase):
     def test_number_only_handling(self):
         self.assertEqual(device.only_number("1qaz2[]3';.4`5'6vtr7ol-8n,j,9][0"), "1234567890")  # add assertion here
 
+    def test_unlocking(self):
+        check = device.check_unlock("839831", 0, False)
+        self.assertTrue(check[1])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_locking(self):
+        check = device.check_unlock("839834", 0, True)
+        self.assertFalse(check[1])
+
