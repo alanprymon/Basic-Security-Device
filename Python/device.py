bff1ba99c301ac79547f9b456b187f35798b4cb9
Alan <aprymon@hawk.iit.edu>
2022-11-22 23:09:38 -0600

    Update device.py

--- Python/device.py
+++ Python/device.py
@@ -12,10 +12,37 @@ def only_number(check):
         delete.clear()
     return checked
 
+def check_unlock(get, idx, unlocked):
+    for x in get:
+        # handle the input string into individual characters
+        if x == "1" and idx == 5:
+            if not unlocked:
+                unlocked = True
+                print("unlocked")
+                idx = 0
+        elif x == "4" and idx == 5:
+            if unlocked:
+                unlocked = False
+                print("locked")
+                idx = 0
+        elif x == "8" and idx == 0 or idx == 3:
+            idx += 1
+        elif x == "3" and idx == 1 or idx == 4:
+            idx += 1
+        elif x == "9" and idx == 2:
+            idx += 1
+        elif x == "8" and idx > 3 or idx < 3:
+            idx = 1
+        elif x == "9" and idx == 5:
+            idx = 3
+        else:
+            idx = 0
+    send = (idx, unlocked)
+    return send
+
 if __name__ == "__main__":
     idx = 0
     unlocked = False
-    # Where main code starts
     while True:
         get = input()
 
